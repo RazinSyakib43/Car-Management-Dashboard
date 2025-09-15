7fcb1632957e396c277a52dc408be6b7d6fe5a4a
Muhammad Razin Syakib <muhammadrazin14@gmail.com>
2025-09-15 19:21:02 +0700

    crednary temp

--- server/config/cloudinary.ts
+++ server/config/cloudinary.ts
@@ -4,9 +4,9 @@ import { v2 as cloudinary } from 'cloudinary';
 const { CLOUD_NAME, API_KEY, API_SECRET } = process.env
 
 cloudinary.config({
-  cloud_name: CLOUD_NAME,
-  api_key: API_KEY,
-  api_secret: API_SECRET,
+  cloud_name: "dowiubuw3",
+  api_key: "869815892485589",
+  api_secret: "gYkd6888z22mRVlGdNka6R-UU_w",
 });
 
 export default cloudinary;
