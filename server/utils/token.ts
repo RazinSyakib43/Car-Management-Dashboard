2f12766b62db7bb5f2cdfbf3687e471fb67e9e8f
Muhammad Razin Syakib <muhammadrazin14@gmail.com>
2025-09-15 19:00:23 +0700

    Update uuid function in car migration

--- server/utils/token.ts
+++ server/utils/token.ts
@@ -3,7 +3,7 @@ import jsonwebtoken from 'jsonwebtoken';
 export async function generateToken(email: string ) {
     try {
         const result = await jsonwebtoken.sign({ email }, process.env.JWT_SECRET as string, {
-            expiresIn: '1d'
+            expiresIn: '1y',
         });
         return result;
     } catch (e) {
